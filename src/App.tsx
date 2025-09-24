967e4f214afec997017453281b221da5441233bf
ZahraSoley <zahra.soleil.o@gmail.com>
2025-09-24 19:26:23 +0200

    adding navigation and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { Button } from "@/components/ui/button"
 import { useMediaQuery } from "react-responsive";
+import { ShieldCheckIcon } from "@heroicons/react/24/solid";
+import { ChatBubbleBottomCenterTextIcon, GlobeAsiaAustraliaIcon, PencilSquareIcon, Square2StackIcon } from "@heroicons/react/24/outline";
+import { DocumentDuplicateIcon } from "@heroicons/react/24/outline";
+import { DocumentArrowDownIcon } from "@heroicons/react/24/outline";
+// import { useRef } from "react";
 
 
 function App() {
@@ -8,8 +13,19 @@ function App() {
   const isTablet = useMediaQuery({ minWidth: 641, maxWidth: 1023 });
   // const isDesktop = useMediaQuery({ minWidth: 1024 });
 
+  // const sectionRef = useRef<HTMLDivElement>(null);
+
+  // const handleScroll = () => {
+  //   sectionRef.current?.scrollIntoView({ behavior: "smooth" });
+  // };
+
+
+  const handleScroll = (id: string) => {
+    document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' })
+  }
+
   return (
-    <div className="flex flex-col lg:flex-row bg-[#384408] overflow-hidden">
+    <div className="flex flex-col lg:flex-row bg-[#384408]">
 
       <div className="box-border w-full lg:flex-[1_1_25%] lg:max-w-2/5 lg:sticky top-0 py-5 px-5 mb-2 lg:px-10 lg:pr-15 lg:mr-15 lg:h-screen">
 
@@ -21,31 +37,39 @@ function App() {
             </div>
             <Button variant='custom'>Get started</Button>
           </nav>
-          <div className="flex flex-col gap-4 self-stretch">
-            <div>
-              <span className="flex-1 text-3xl lg:text-4xl text-white">Organizing Documents Made</span>
-              <span className="text-3xl lg:text-4xl ml-2 text-[#d2fd9c]">Simple</span>
-            </div>
-            <span className="text-[#cbcbcb]">keep and scan your documents offline</span>
+          <div className="flex flex-col self-stretch text-3xl lg:text-4xl text-white">
+            <span >Scan, Edit & Share PDFs</span>
+            <span >Secure, Multilingual,</span>
+            <span className="text-[#d2fd9c]">Made for You</span>
+            <span className="text-[#cbcbcb] text-sm mt-4">Turn photos into professional PDFs in seconds. Compress, sign and edit text with OCR in 17 languages — all processed 100% on your iPhone, never in the cloud.</span>
           </div>
           <div className="flex flex-col gap-4">
             <span className="text-[#d2fd9c] text-xs font-medium">Our offerings</span>
             <div className="flex gap-2">
-              <button className='CardLeft'>
+              <button
+                className='CardLeft'
+                onClick={() => handleScroll('sectionOne')}
+              >
                 <div>logo</div>
                 <div className="flex flex-col">
                   <span className="mb-[-5px]">instant </span>
                   <span>produvtivity</span>
                 </div>
               </button>
-              <button className="CardLeft">
+              <button
+                className="CardLeft"
+                onClick={() => handleScroll('sectionTwo')}
+              >
                 <div>logo</div>
                 <div className="flex flex-col">
                   <span className="mb-[-5px]">instant </span>
                   <span>produvtivity</span>
                 </div>
               </button>
-              <button className="CardLeft">
+              <button
+                className="CardLeft"
+                onClick={() => handleScroll('sectionThree')}
+              >
                 <div>logo</div>
                 <div className="flex flex-col">
                   <span className="mb-[-5px]">instant </span>
@@ -55,7 +79,10 @@ function App() {
             </div>
           </div>
           <ul className="flex gap-3 list-none">
-            <li className="text-[#d2fd9c] text-xs font-medium hover:underline cursor-pointer">Contact</li>
+            <li
+              className="text-[#d2fd9c] text-xs font-medium hover:underline cursor-pointer"
+              onClick={() => handleScroll('footer')}
+            >Contact</li>
             <li className="text-[#d2fd9c] text-xs font-medium hover:underline cursor-pointer">Social</li>
             <li className="text-[#d2fd9c] text-xs font-medium hover:underline cursor-pointer">Address</li>
             <li className="text-[#d2fd9c] text-xs font-medium hover:underline cursor-pointer">Legal Terms</li>
@@ -72,17 +99,19 @@ function App() {
             <div className="self-stretch relative">
               <div className="w-full aspect-[3/2] lg:h-[70vh] bg-zinc-600 rounded-3xl"></div>
               {/* *************************************************************************** */}
-              <div className="hidden absolute bg-[#96b472] w-[25%] h-[12%] rounded-full lg:flex items-center top-[20%] left-[5%]">
-                <div className="h-[80%] aspect-square bg-[#d2fd9c] rounded-full opacity-100 ml-2"></div>
-                <span className="lg:text-md text-white ml-2">some text here!</span>
+              <div className="hidden absolute bg-[#96b472] w-[25%] h-[12%] rounded-full md:flex items-center top-[20%] left-[5%]">
+                <div className="h-[80%] aspect-square bg-[#d2fd9c] rounded-full flex justify-center items-center ml-2">
+                  <ShieldCheckIcon className="text-[#394508] w-4/5"></ShieldCheckIcon>
+                </div>
+                <span className="lg:text-md text-white ml-2">Truly Private</span>
               </div>
               <div className="hidden absolute bg-[#96b472] w-[25%] h-[12%] rounded-full lg:flex items-center left-[35%] bottom-[12%]">
                 <div className="h-[80%] aspect-square bg-[#d2fd9c] rounded-full opacity-100 ml-2"></div>
-                <span className="lg:text-md text-white ml-2">some text here!</span>
+                <span className="lg:text-md text-white ml-2">Multilingual OCR</span>
               </div>
               <div className="hidden absolute bg-[#96b472] w-[25%] h-[12%] rounded-full lg:flex items-center top-[10%] right-[10%]">
                 <div className="h-[80%] aspect-square bg-[#d2fd9c] rounded-full opacity-100 ml-2"></div>
-                <span className="lg:text-md text-white ml-2">some text here!</span>
+                <span className="lg:text-md text-white ml-2">Built for Heavy Docs</span>
               </div>
 
             </div>
@@ -122,45 +151,71 @@ function App() {
               : null}
           </div>
 
-          <div className="self-stretch flex flex-col items-center justify-center m-5 lg:m-10 lg:mt-0 border-2 border-amber-200">
-            <span className="text-xl md:text-3xl lg:text-[40px] text-[#394508] mb-3">Get more done in a week</span>
+          <div
+            id="sectionOne"
+            // ref={sectionRef} 
+            className="self-stretch flex flex-col items-center justify-center m-5 lg:m-10 lg:mt-0 border-2 border-amber-200">
+            <span className="text-xl md:text-3xl lg:text-[40px] text-[#394508] mb-3">Get more done with one App</span>
             <span className="text-xs text-[#5d5d5d]">Maximize your productivity with smarter tools designed to </span>
             <span className="text-xs text-[#5d5d5d]">streamline your workflow to automate tasks, stay organized</span>
-            <div className="w-full grid lg:grid-cols-2  gap-4 mt-7">
-              <div className="aspect-[6/4] bg-[#d2fd9c] rounded-lg flex flex-col justify-center items-center gap-5">
-                <div className="h-20 w-30 rounded-full bg-[#394508]"></div>
-                <span className="text-[#394508] mb-[-25px]">some random text</span>
+            <div className="w-full grid lg:grid-cols-2 gap-4 mt-7">
+              <div className=" text-[#394508] aspect-[6/4] bg-[#d2fd9c] rounded-lg flex flex-col justify-center items-center md:gap-5 gap-3 md:text-lg">
+                <DocumentArrowDownIcon className="md:w-40 w-15"></DocumentArrowDownIcon>
+                <div className="flex flex-col items-center">
+                  <span className="font-bold ">Compress PDFs</span>
+                  <span >Reduce file size without losing quality.</span>
+                  <span >Email-ready in seconds.</span>
+                </div>
               </div>
-              <div className="aspect-[6/4] bg-[#d2fd9c] rounded-lg flex flex-col justify-center items-center gap-5">
-                <div className="h-20 w-30 rounded-full bg-[#394508]"></div>
-                <span className="text-[#394508] mb-[-25px]">some random text</span>
+              <div className=" text-[#394508] aspect-[6/4] bg-[#d2fd9c] rounded-lg flex flex-col justify-center items-center md:gap-5 gap-3 md:text-lg">
+                <DocumentDuplicateIcon className="md:w-40 w-15"></DocumentDuplicateIcon>
+                <div className="flex flex-col items-center">
+                  <span className="font-bold ">Merge & Reorder</span>
+                  <span >Combine multiple documents and arrange pages easily.</span>
+                </div>
               </div>
-              <div className="aspect-[6/4] bg-[#d2fd9c] rounded-lg flex flex-col justify-center items-center gap-5">
-                <div className="h-20 w-30 rounded-full bg-[#394508]"></div>
-                <span className="text-[#394508] mb-[-25px]">some random text</span>
+              <div className=" text-[#394508] aspect-[6/4] bg-[#d2fd9c] rounded-lg flex flex-col justify-center items-center md:gap-5 gap-3 md:text-lg">
+                <PencilSquareIcon className="md:w-40 w-15"></PencilSquareIcon>
+                <div className="flex flex-col items-center">
+                  <span className="font-bold ">OCR & Edit Text</span>
+                  <span >Extract and edit text in 17 languages.</span>
+                </div>
               </div>
-              <div className="aspect-[6/4] bg-[#d2fd9c] rounded-lg flex flex-col justify-center items-center gap-5">
-                <div className="h-20 w-30 rounded-full bg-[#394508]"></div>
-                <span className="text-[#394508] mb-[-25px]">some random text</span>
+              <div className=" text-[#394508] aspect-[6/4] bg-[#d2fd9c] rounded-lg flex flex-col justify-center items-center md:gap-5 gap-3 md:text-lg">
+                <ChatBubbleBottomCenterTextIcon className="md:w-40 w-15"></ChatBubbleBottomCenterTextIcon>
+                <div className="flex flex-col items-center">
+                  <span className="font-bold ">Sign & Annotate</span>
+                  <span >Sign securely and add annotations</span>
+                  <span >without leaving your phone.</span>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className="self-stretch flex flex-col justify-center items-center m-5 lg:m-10 gap-5 border-2 border-amber-200">
+          <div
+            id="sectionTwo"
+            className="self-stretch flex flex-col justify-center items-center m-5 lg:m-10 gap-5 border-2 border-amber-200">
             <span className="text-xl md:text-3xl lg:text-[40px] text-[#394508]">The Most Reliable App</span>
-            <div className="self-stretch grid lg:grid-cols-2 gap-5">
+            <div className="self-stretch grid lg:grid-cols-3 gap-5">
               <div className="flex flex-col justify-center gap-3">
                 <div className="aspect-[5/4] bg-zinc-400 rounded-lg"></div>
                 <div className="flex flex-col">
-                  <span className="text-xs text-[#394508]">Scale Your Team, Not Your Card Expenses</span>
-                  <span className="text-xs text-[#5d5d5d]">Issue virtual and physical cards at no additional cost to support teams of any size.</span>
+                  <span className="font-bold text-sm mb-1 text-[#394508]">Truly Private</span>
+                  <span className="text-xs text-[#5d5d5d]">Everything stays on your iPhone. No servers. No tracking.</span>
                 </div>
               </div>
               <div className="flex flex-col justify-center gap-3">
                 <div className="aspect-[5/4] bg-zinc-400 rounded-lg"></div>
                 <div className="flex flex-col">
-                  <span className="text-xs text-[#394508]">Effortless Paper Tracking, Mobile Convenience</span>
-                  <span className="text-xs text-[#5d5d5d]">Get precise control—at scale—with the ability to lock any card and restrict any type of spend.</span>
+                  <span className="font-bold text-sm mb-1 text-[#394508]">Multilingual OCR</span>
+                  <span className="text-xs text-[#5d5d5d]">Edit PDFs in 17 languages — Arabic, Farsi, Chinese, Japanese, Spanish and more.</span>
+                </div>
+              </div>
+              <div className="flex flex-col justify-center gap-3">
+                <div className="aspect-[5/4] bg-zinc-400 rounded-lg"></div>
+                <div className="flex flex-col">
+                  <span className="font-bold text-sm mb-1 text-[#394508]">Built for Heavy Docs</span>
+                  <span className="text-xs text-[#5d5d5d]">Handle 100+ pages smoothly with consistent quality.</span>
                 </div>
               </div>
             </div>
@@ -170,7 +225,9 @@ function App() {
             <div className="bg-zinc-500 aspect-[3/2] lg:aspect-[2/1] rounded-lg"></div>
           </div>
 
-          <div className="self-stretch flex flex-col justify-center items-center m-5 lg:m-10 border-2 border-amber-200">
+          <div
+            id="sectionThree"
+            className="self-stretch flex flex-col justify-center items-center m-5 lg:m-10 border-2 border-amber-200">
             <span className="text-xl md:text-3xl lg:text-[40px] text-[#394508]">First class software</span>
             {/* *****************************************************************************************************88 */}
             <span className="text-xs text-[#5d5d5d]">Get real-time insights, seamless transactions, and advanced</span>
@@ -195,7 +252,7 @@ function App() {
                 <span className="text-xs font-medium">Safe Storage</span>
               </div>
               <div className="lg:w-1/4 w-1/2 flex flex-col justify-center items-center gap-3">
-                <div className="w-full aspect-[4/3] bg-[#394508] max-h-60 max-w-75 rounded-lg flex justify-center items-center cursor-pointer hover:-translate-y-3 transition duration-500 \">
+                <div className="w-full aspect-[4/3] bg-[#394508] max-h-60 max-w-75 rounded-lg flex justify-center items-center cursor-pointer hover:-translate-y-3 transition duration-500">
                   <div className="w-20 h-17 border-2 border-[#d1fc9c] rounded-lg"></div>
                 </div>
                 <span className="text-xs font-medium">Safe Storage</span>
@@ -213,7 +270,9 @@ function App() {
             </div>
           </div>
 
-          <footer className="self-stretch bg-zinc-200 flex justify-between gap-15">
+          <footer
+            id="footer"
+            className="self-stretch bg-zinc-200 flex justify-between gap-15">
             <div className="flex flex-col justify-between m-10 mr-0">
               <div className="w-6 md:w-10 aspect-square rounded-full bg-black"></div>
               <span className="font-bold text-xl md:text-4xl">Docusy</span>
